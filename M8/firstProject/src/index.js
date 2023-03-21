ff0c3847effc1831310cd583a322a4ea43829ea4
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-03-21 13:48:54 +0200

    add less-15

--- M8/firstProject/src/index.js
+++ M8/firstProject/src/index.js
@@ -1,21 +1,22 @@
-// import { fn, fn3 } from './generateRandomImg'
-// import generateRandomImg from './generateRandomImg'
-import generateRandomImg from './scripts/generateRandomImg'
-// import * as props from './generateRandomImg'
+// // import { fn, fn3 } from './generateRandomImg'
+// // import generateRandomImg from './generateRandomImg'
+// import generateRandomImg from './scripts/generateRandomImg'
+// // import * as props from './generateRandomImg'
 
-// props.default()
-// props.fn()
-import './styles/style.css'
-import './styles/styles.scss'
+// // props.default()
+// // props.fn()
+// import './styles/style.css'
+// import './styles/styles.scss'
 
-import _ from 'lodash'
+// import _ from 'lodash'
 
-const main = document.querySelector('.main')
-const randomLoopNumber = Math.round(Math.random(0, 1) * 100)
+// const main = document.querySelector('.main')
+// const randomLoopNumber = Math.round(Math.random(0, 1) * 100)
 
-for (let i = 0; i < randomLoopNumber; i++) {
-	main.append(generateRandomImg())
-}
+// for (let i = 0; i < randomLoopNumber; i++) {
+// 	main.append(generateRandomImg())
+// }
 
-function fn({ name, age }) {}
-console.log('object')
+// function fn({ name, age }) {}
+// console.log('object')
+import './scripts/less_15/app'
